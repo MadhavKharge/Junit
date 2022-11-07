d1720e235c79e4c783c7ac94a5fdab5f7cac49ff
madhavkharge <madhavkharge@localhost>
2022-11-07 12:22:51 +0530

    First commit...

---
+++ src/AnnotationDemo.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class AnnotationDemo {
+
+    WebDriver driver;
+    
+    @BeforeClass
+    public void setup(){
+    	
+	   
+   }
+	}
